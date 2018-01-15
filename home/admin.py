cd1ae21e7087cf034d3de0d848101be59d1b7e6d
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-01-15 21:57:53 +0000

    Second commit
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Author, BlogPost
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(BlogPost)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    pub_date = models.DateField('date published')
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.title
