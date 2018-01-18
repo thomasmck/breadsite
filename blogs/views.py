00f18cef8098a91fda6b902aab898ce08edff39a
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-01-18 16:50:38 +0000

    Test graph code
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
 from home.models import BlogPost
 
@@ -9,10 +9,6 @@ def index(request):
     return HttpResponse("Hello, world. The latest blog is %s." %latest_blog)
 
 def detail(request, blogpost_id):
-    blog = BlogPost.objects.get(id=blogpost_id)
-    print(blog)
-    template = loader.get_template('blogs/detail.html')
-    context = {
-        'blog': blog,
-    }
-    return HttpResponse(template.render(context, request))
+    blog = get_object_or_404(BlogPost, id=blogpost_id)
+    context = {'blog': blog}
+    return render(request, 'blogs/detail.html', context)
--- home/views.py
+++ home/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import BlogPost, Author
+from matplotlib import pylab
+from pylab import *
+import PIL, PIL.Image, io
 
+def getImage(request):
+    # Construct the graph
+    x = arange(0, 2*pi, 0.01)
+    s = cos(x)**2
+    plot(x, s)
+
+    xlabel('xlabel(X)')
+    ylabel('ylabel(Y)')
+    title('Simple Graph!')
+    grid(True)
+
+    # Store image in a string buffer
+    buffer = io.BytesIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer, "PNG")
+    pylab.close()
+    return buffer
 
 def index(request):
     latest_blogs_list = BlogPost.objects.order_by('pub_date')[:5]
@@ -10,4 +32,6 @@ def index(request):
     context = {
         'latest_blogs_list': latest_blogs_list,
     }
-    return HttpResponse(template.render(context, request))
+    buffer = getImage(request)
+    # return HttpResponse(template.render(context, request))
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
