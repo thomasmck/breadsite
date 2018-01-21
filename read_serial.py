8ebd36c2963df1192b22875a69c0665cfb1f23e1
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-01-21 21:38:22 +0000

    Adding code to read serial output from the arduino
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

---
+++ read_serial.py
@@ -0,0 +1,8 @@
+import serial
+ 
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate=9600
+
+while True:
+    read_ser=ser.readline()
+    print(read_ser)
