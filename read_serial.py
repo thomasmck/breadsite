e24b7f0f6b483419d384a67516388b5c96d3cf35
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-01-22 07:17:54 +0000

    Write serial outpuerial output to db
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- read_serial.py
+++ read_serial.py
@@ -1,8 +1,20 @@
 import serial
- 
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+import sqlite3
+from datetime import datetime 
+import sleep
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
+
+ser=serial.Serial("/dev/ttyACM0",9600)
 ser.baudrate=9600
 
+
 while True:
-    read_ser=ser.readline()
+    read_ser=int(ser.readline())
     print(read_ser)
+    c.execute('INSERT INTO home_temperature VALUES (?,?)', (read_ser, datetime.now()))
+    c.commit()
+    sleep(2)
+
+conn.close()
