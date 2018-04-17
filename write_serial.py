d22af1c873b79606258bcee94557248c3ee38278
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-04-17 06:12:19 +0000

    Adding seperate file for reading or reading/writing from serial for debugging purposes
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- write_serial.py
+++ write_serial.py
@@ -1,6 +1,10 @@
 import serial
+import sqlite3
 from datetime import datetime 
 from time import sleep
+
+conn = sqlite3.connect('db.sqlite3')
+c = conn.cursor()
 
 ser=serial.Serial("/dev/ttyACM0",9600)
 ser.baudrate=9600
@@ -10,4 +14,7 @@ while True:
     read_ser=float(ser.readline())
     print(read_ser)
     print(datetime.now())
+    c.execute('INSERT INTO home_temperature (rec_date, temp) VALUES (?,?)', (datetime.now(), read_ser))
+    conn.commit()
 
+conn.close()
