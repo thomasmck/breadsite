65b69e31dda420830c515f6c9d2a4cd3cab54126
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2018-09-28 18:20:03 +0100

    Minor react addition
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Square(props) {
+  return (
+    <button className="square">{props.value}</button>
+  );
+}
+
 const App = () => (
-  <p>THIS IS A TEST</p>
+  <div>
+    <p>THIS IS A TEST</p>
+  </div>
+  <div>
+    <Square value="tom_test"/>
+  </div>
+
 );
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
