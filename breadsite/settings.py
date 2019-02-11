b957c65eb59a5fd6ffe8f4cc6b239a45f94244c1
Thomas Mckelvey <thomas.mckelvey@citrix.com>
2019-02-11 20:25:23 +0000

    Settings update
    Signed-off-by: Thomas Mckelvey <thomas.mckelvey@citrix.com>

--- breadsite/settings.py
+++ breadsite/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'd%h=cxkks5mn$cwzvbt47lfvz)p)w+a^49f)gu3tl26s7^snko'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.69', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.69', 'localhost', '127.0.0.1', '192.168.0.24']
 
 # Application definition
 
